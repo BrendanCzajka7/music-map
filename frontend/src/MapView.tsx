33103e363c7a12844d0ceae01f8058c730cf31f1
Brendan <brendanczajka77@gmail.com>
2026-03-08 13:40:31 -0500

    icon patch

--- frontend/src/MapView.tsx
+++ frontend/src/MapView.tsx
@@ -2,12 +2,22 @@ import { useEffect, useRef } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
 type Memory = {
   lat: number;
   lng: number;
   song: string;
   text: string;
 };
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 export default function MapView() {
   const mapRef = useRef<HTMLDivElement | null>(null);
